e7986a377b0d92b1c240fb11c2b76f1e2a5e0625
Will Lines <williamlines42@gmail.com>
2024-11-30 15:51:28 +0000

    fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,21 @@
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
-import prettierPlugin from 'eslint-config-prettier'
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import pluginJest from 'eslint-plugin-jest'
 
 export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
-    ignores: ['**/node_modules/**', './coverage/**'],
-    files: ['**/*.js'],
-    plugins: {
-      '@typescript-eslint': typescriptPlugin,
-      prettier: prettierPlugin,
+    files: ['**/*.test.{js,mjs,cjs,ts}'],
+    plugins: { jest: pluginJest },
+    ...pluginJest.configs['flat/recommended'],
+    rules: {
+      ...pluginJest.configs['flat/recommended'].rules,
     },
   },
 ]
